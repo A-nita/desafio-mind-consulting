f0aa3efc7e465f922cf132b4ba3ffcf7711426b9
A_nita <46121612+A-nita@users.noreply.github.com>
2023-12-01 17:34:45 -0300

    Refactor: user query to only search by email

--- api/src/repository/users/postgres-find-user.ts
+++ api/src/repository/users/postgres-find-user.ts
@@ -7,8 +7,8 @@ export class PostgresFindUserRepository implements IFindUserRepository {
   async findUser(user: UserInput): Promise<User> {
 
     const res = await db.query(
-      'SELECT * FROM users WHERE email = ($1) AND password = ($2)',
-      [user.email, user.password]
+      'SELECT * FROM users WHERE email = ($1)',
+      [user.email]
     );
     return res.rows[0];
   }
