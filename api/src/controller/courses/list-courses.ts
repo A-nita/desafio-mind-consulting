fbc116a89651167c389bc3d5be187f04e05de6bd
A_nita <46121612+A-nita@users.noreply.github.com>
2023-12-02 08:09:00 -0300

    fix: listCourses to include professor's name

--- api/src/controller/courses/list-courses.ts
+++ api/src/controller/courses/list-courses.ts
@@ -12,6 +12,7 @@ export class ListCoursesController implements IListCoursesController {
         body: courses,
       };
     } catch (error) {
+      console.log(error);
       return {
         statusCode: 500,
         body: 'Something went wrong',
--- api/src/repository/courses/postgres-list-courses.ts
+++ api/src/repository/courses/postgres-list-courses.ts
@@ -4,7 +4,15 @@ import db from '../../db';
 
 export class PostgresListCoursesRepository implements IListCoursesRepository {
   async listCourses(): Promise<Course[]> {
-    const res = await db.query('SELECT * FROM courses', []);
+    const res = await db.query(
+      'SELECT courses.id,courses.title,users.first_name,users.last_name,category.title AS category,courses.description,courses.image,courses.active FROM courses INNER JOIN category ON category.id = courses.category_id INNER JOIN users ON users.id = courses.professor_id ORDER BY  courses.active DESC,  courses.title', []);
+    // para cada linha juntar first name com last name em professor
+
+    for (let i = 0; i < res.rows.length; i++) {
+      res.rows[i].professor = res.rows[i].first_name + ' ' + res.rows[i].last_name;
+      delete res.rows[i].first_name;
+      delete res.rows[i].last_name;
+    }
     return res.rows;
   }
 }
