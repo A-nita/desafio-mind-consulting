f8f1bdbc218c5fd5e40549c6bf45110dcdf77958
A_nita <46121612+A-nita@users.noreply.github.com>
2023-12-02 13:12:13 -0300

    Refactor: login page layout and styles

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,19 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Root() {
   return (
     // Pagina de Login
     <div className="mainContainer">
-      <div className={"titleContainer"}>
-        <div>Welcome!</div>
+      <div className="welcome">
+         <h1>Seja bem-vindo à plataforma que transforma conhecimento em conquistas!</h1> 
       </div>
-      <div>
-        This is the home page.
-      </div>
-      <div className={"buttonContainer"}>
-        <input
-          className={"inputButton"}
-          type="button"
-          onClick={() => console.log('click')}
-          value={'Log in'} />
+      <div className="login">
+        <h1>/Mind Cursos</h1>        
+        <form className="form-login" action="localhost:3000/login" method="post">
+          <h2>Login</h2>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+          <label htmlFor="password">Senha</label>
+          <input type="password" id="password" />
+          <button type="submit">Entrar?</button>        
+        </form>
       </div>
     </div>
   );
