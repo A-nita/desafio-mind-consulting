273a3beeafd74225b8dcb922e668674e98f83205
A_nita <46121612+A-nita@users.noreply.github.com>
2023-12-02 20:10:51 -0300

    feat: Passport w/ repositry for authentication

--- api/src/utils/passport.ts
+++ api/src/utils/passport.ts
@@ -2,13 +2,21 @@ import passport from 'passport';
 import { Strategy as JwtStrategy } from 'passport-jwt';
 import { ExtractJwt } from 'passport-jwt';
 
-export default () => {
+import { IFindUserRepository } from '../repository/users/interfaces';
+
+export default (findUserRepository: IFindUserRepository) => {
   const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET,
   };
 
-  passport.use('jwt', new JwtStrategy(opts, function(jwtPayload, done) {
-    return done(null, jwtPayload);
+  passport.use('jwt', new JwtStrategy(opts, async function(jwtPayload, done) {
+    const user = await findUserRepository.findUser({ email: jwtPayload.email });
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, { id: user.id, email: user.email, admin: user.admin, name: user.name });
   }));
 };
